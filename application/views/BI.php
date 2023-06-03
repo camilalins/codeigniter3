ac7fe4387efbf83836efae417b2b93683dc0784c
srtab <gmlins06@gmail.com>
2023-06-03 13:13:40 -0300

    alterado as informações para exibição do novo template

---
+++ application/views/BI.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<!-- cabeçalho do template -->
+</head>
+<body>
+<!-- conteúdo do template -->
+<div class="content">
+	<h1>Bem-vindo ao meu site</h1>
+	<!-- outros conteúdos específicos do CodeIgniter -->
+</div>
+<!-- rodapé do template -->
+</body>
+</html>
